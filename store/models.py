08c091a575c2144d6349ea3d4c4ab987eb2a838e
mr azad <ali.azadmanjiri2000@gmail.com>
2023-11-29 13:10:53 +0330

    create store.Product model

--- store/models.py
+++ store/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 
 class Category(models.Model):
@@ -7,4 +8,14 @@ class Category(models.Model):
     
     def __str__(self):
         return self.title
-    
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='products')
+    slug = models.SlugField()
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField(validators=[MinValueValidator(0)])
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)

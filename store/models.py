1310c254d4fcce4ee3e59cac63dd4b70c1b3eb7a
mr azad <ali.azadmanjiri2000@gmail.com>
2023-12-01 18:56:59 +0330

    create store.CartItem model

--- store/models.py
+++ store/models.py
@@ -95,3 +95,12 @@ class OrderItem(models.Model):
 class Cart(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4)
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='cart_items')
+    quantity = models.PositiveSmallIntegerField()
+
+    class Meta:
+        unique_together = [['cart', 'product'], ]

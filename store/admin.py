30ce6210f87752156410ee8f5ca8c5b582e929de
mr azad <ali.azadmanjiri2000@gmail.com>
2023-12-03 21:06:29 +0330

    register store.Customer model & create store.CustomerAdmin in admin panel

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Customer
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'email', ]
+    ordering = ['user__last_name',]
+    search_fields = ['user__last_name__istartswith',]
+
+    def email(self, customer):
+        return customer.user.email
+        

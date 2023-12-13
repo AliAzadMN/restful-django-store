84fc2e1be2c685bd7f1d093a28faa0cdac9285c8
mr azad <ali.azadmanjiri2000@gmail.com>
2023-12-13 09:55:03 +0330

    create store.CreateUpdateCategorySerializer

--- store/serializers.py
+++ store/serializers.py
@@ -9,3 +9,15 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Category
         fields = ['id', 'title', 'description', 'num_of_products', ]
+
+
+class CreateUpdateCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Category
+        fields = ['title', 'description', ]
+
+    def validate(self, data):
+        title = data.get('title')
+        if title and len(title) < 3:
+            return serializers.ValidationError("Product title length should be at least 3")
+        return data
